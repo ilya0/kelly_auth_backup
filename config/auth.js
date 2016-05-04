b3a371d64ca83cd40c899598ebb61bba639ef8bd
ilya osovets <iosovets@gmail.com>
2016-05-03 17:09:15 -0700

    init commit

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+
+    'googleAuth' : {
+        'clientID'         : '209607909142-rl33svnu6k52maq19mljojti8u8vq7bb.apps.googleusercontent.com',
+        'clientSecret'     : 'CwCI0goeX3Tc0vud8DCpPuzE',
+        'callbackURL'      : 'http://localhost:8080/connect/google/callback/'
+    }
+
+};
+
+
